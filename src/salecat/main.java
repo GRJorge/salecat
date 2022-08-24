89d9b91aa973cd312bac1c266a757715f117be31
axdevil <deb.devel@outlook.es>
2022-08-23 20:12:25 -0500

    Test

--- src/salecat/main.java
+++ src/salecat/main.java
@@ -1,16 +1,26 @@
 package salecat;
 
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.IOException;
 /**
  *
- * @author jorge
+ * @author axdevil
  */
 public class main {
 
     /**
      * @param args the command line arguments
+     * @throws java.io.IOException
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException {
+        
+        String contentTicket;
+        
+        BufferedReader scanTicket = new BufferedReader(new FileReader("/home/jorge/Documentos/Proyectos/Java/salecat/ticket.txt"));
+        BufferedWriter editTicket = new BufferedWriter(new FileWriter("/home/jorge/Documentos/Proyectos/Java/salecat/ticket.txt"));
     }
     
 }
