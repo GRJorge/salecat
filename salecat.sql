cebe1315a5fc5807f9823e2870fd6361d15288fb
axdevil <deb.devel@outlook.es>
2022-09-06 08:36:09 -0500

    Database archive

---
+++ salecat.sql
@@ -0,0 +1,193 @@
+-- MariaDB dump 10.19  Distrib 10.9.2-MariaDB, for Linux (x86_64)
+--
+-- Host: localhost    Database: salecat
+-- ------------------------------------------------------
+-- Server version	10.9.2-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `permit`
+--
+
+DROP TABLE IF EXISTS `permit`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `permit` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `product` tinyint(1) NOT NULL,
+  `sale` tinyint(1) NOT NULL,
+  `inventory` tinyint(1) NOT NULL,
+  `provider` tinyint(1) NOT NULL,
+  `userFK` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `userFK` (`userFK`),
+  CONSTRAINT `permit_ibfk_1` FOREIGN KEY (`userFK`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `permit`
+--
+
+LOCK TABLES `permit` WRITE;
+/*!40000 ALTER TABLE `permit` DISABLE KEYS */;
+/*!40000 ALTER TABLE `permit` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `code` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `price` int(11) NOT NULL DEFAULT 0,
+  `wholesalePrice` int(11) NOT NULL DEFAULT 0,
+  `gain` int(11) NOT NULL DEFAULT 0,
+  `dateCreation` date NOT NULL DEFAULT curdate(),
+  `providerFK` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `providerFK` (`providerFK`),
+  CONSTRAINT `product_ibfk_1` FOREIGN KEY (`providerFK`) REFERENCES `provider` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product`
+--
+
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `productSale`
+--
+
+DROP TABLE IF EXISTS `productSale`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `productSale` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `productFK` int(11) NOT NULL,
+  `saleFK` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `productFK` (`productFK`),
+  KEY `saleFK` (`saleFK`),
+  CONSTRAINT `productSale_ibfk_1` FOREIGN KEY (`productFK`) REFERENCES `product` (`id`),
+  CONSTRAINT `productSale_ibfk_2` FOREIGN KEY (`saleFK`) REFERENCES `sale` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `productSale`
+--
+
+LOCK TABLES `productSale` WRITE;
+/*!40000 ALTER TABLE `productSale` DISABLE KEYS */;
+/*!40000 ALTER TABLE `productSale` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `provider`
+--
+
+DROP TABLE IF EXISTS `provider`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `provider` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `appat` varchar(32) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `apmat` varchar(32) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `business` varchar(64) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `telefono` varchar(16) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `provider`
+--
+
+LOCK TABLES `provider` WRITE;
+/*!40000 ALTER TABLE `provider` DISABLE KEYS */;
+/*!40000 ALTER TABLE `provider` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sale`
+--
+
+DROP TABLE IF EXISTS `sale`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sale` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `date` date NOT NULL DEFAULT curdate(),
+  `total` int(11) NOT NULL,
+  `userFK` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `userFK` (`userFK`),
+  CONSTRAINT `sale_ibfk_1` FOREIGN KEY (`userFK`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sale`
+--
+
+LOCK TABLES `sale` WRITE;
+/*!40000 ALTER TABLE `sale` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sale` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-09-06  8:32:51
